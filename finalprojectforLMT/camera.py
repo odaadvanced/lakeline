f60d77b2cbb53b0e972987c488938f14d223b15c
adaadvanced <robots@global.com>
2025-05-01 15:29:25 -0500

    hh

--- finalprojectforLMT/camera.py
+++ finalprojectforLMT/camera.py
@@ -4,8 +4,9 @@ from time import sleep
 camera = picamera.PiCamera()
 
 def picture(directory):
+    '''The picture(directory) function takes a picture using the camera and saves it to the 'directory' directory.'''
     camera.start_preview()
-    sleep(5)
+    sleep(1)
     camera.capture(directory)
     camera.stop_preview()
     
