a88830d99be10b979a5ffd143da14ae3f74358f7
odaadvanced <robots@stbaglobal.com>
2025-05-15 14:30:22 -0500

    project

---
+++ david_jonathan_project/camera.py
@@ -0,0 +1,25 @@
+
+from picamera import PiCamera
+import os
+user = os.getlogin()
+user_home = os.path.expanduser(f'~{user}')
+
+camera = PiCamera()
+
+def setup():
+    camera.start_preview(alpha=200)
+
+def main():
+    camera.capture(f'{user_home}/my_photo.jpg')
+    while True:
+        pass
+
+def destroy():
+    camera.stop_preview()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        main()
+    except KeyboardInterrupt:
+        destroy()
